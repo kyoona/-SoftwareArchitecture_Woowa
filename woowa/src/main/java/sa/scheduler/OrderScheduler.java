7aa6ad6dfb426f57dd284123d80aef33fd80ce71
kyoona <yoona405@konkuk.ac.kr>
2024-11-28 15:23:50 +0900

    feat : scheduler프로토 타입 개발

---
+++ woowa/src/main/java/sa/scheduler/OrderScheduler.java
@@ -0,0 +1,35 @@
+package sa.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ScheduledFuture;
+
+@RequiredArgsConstructor
+@Component
+public class OrderScheduler {
+
+    private final TaskScheduler scheduler;
+    private final ConcurrentHashMap<Long, ScheduledFuture> orderTask = new ConcurrentHashMap<>();
+
+    public void reserve(Long orderId){
+//        ScheduledFuture<?> future = scheduler.scheduleWithFixedDelay(()->{}, delayTime);
+//        orderTask.put(orderId, future);
+    }
+
+    public void cancel(Long orderId){
+        ScheduledFuture future = orderTask.get(orderId);
+        if (future != null) {
+            future.cancel(true);
+            orderTask.remove(orderId);
+        }
+    }
+
+    private Duration getDuration(LocalDateTime time){
+        return Duration.between(LocalDateTime.now(), time);
+    }
+}
