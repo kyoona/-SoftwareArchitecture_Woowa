2c6d6e53460b746f2b1f9ab0ae53da6c06ab0522
kyoona <yoona405@konkuk.ac.kr>
2024-11-28 15:42:58 +0900

    feat : Location에 생성자 추가

--- common/src/main/java/sa/domain/Location.java
+++ common/src/main/java/sa/domain/Location.java
@@ -1,7 +1,12 @@
 package sa.domain;
 
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Embeddable
 public class Location {
 
