0964e134077f9b0a6ee2df42345f19a0584261ad
dldyou <chankyu2003@gmail.com>
2024-12-04 00:13:23 +0900

    feat: 기사 등록 요청, 승인, 거절 개발

--- delivery/src/main/java/sa/service/DeliveryManService.java
+++ delivery/src/main/java/sa/service/DeliveryManService.java
@@ -3,8 +3,10 @@ package sa.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import sa.domain.Location;
-import sa.domain.User;
+import sa.domain.*;
+import sa.dto.DeliveryManRequestDto;
+import sa.repository.DeliveryManRepository;
+import sa.repository.DeliveryManRequestInfoRepository;
 import sa.repository.UserRepository;
 
 @RequiredArgsConstructor
@@ -12,4 +14,54 @@ import sa.repository.UserRepository;
 @Service
 public class DeliveryManService {
     private final UserRepository userRepository;
+    private final DeliveryManRepository deliveryManRepository;
+    private final DeliveryManRequestInfoRepository deliveryManRequestInfoRepository;
+
+    @Transactional
+    public Long requestDeliveryMan(Long userId, DeliveryManRequestDto deliveryManRequestDto) {
+        User user = userRepository.findById(userId).orElseThrow();
+        checkManager(user);
+
+        User deliveryMan = User.create(deliveryManRequestDto.getDeliveryManName(), deliveryManRequestDto.getLocation(), UserRole.DELIVERYMAN);
+        userRepository.save(deliveryMan);
+
+        return deliveryMan.getId();
+    }
+
+    @Transactional
+    public Long acceptDeliveryMan(Long userId, Long requestId) {
+        User user = userRepository.findById(userId).orElseThrow();
+        checkDeliveryMan(user);
+
+        DeliveryManRequestInfo deliveryManRequestInfo = deliveryManRequestInfoRepository.findById(requestId).orElseThrow();
+        deliveryManRequestInfo.setStatus(DeliveryManRequestStatus.ACCEPT);
+
+        DeliveryMan deliveryMan = deliveryManRequestInfo.createDeliveryMan();
+        deliveryManRepository.save(deliveryMan);
+
+        return deliveryMan.getId();
+    }
+
+    @Transactional
+    public Long denyDeliveryMan(Long userId, Long requestId) {
+        User user = userRepository.findById(userId).orElseThrow();
+        checkManager(user);
+
+        DeliveryManRequestInfo deliveryManRequestInfo = deliveryManRequestInfoRepository.findById(requestId).orElseThrow();
+        deliveryManRequestInfo.setStatus(DeliveryManRequestStatus.DENY);
+
+        return requestId;
+    }
+
+    public void checkManager(User user) {
+        if(user.getUserRole() != UserRole.MANAGER) {
+            throw new RuntimeException();
+        }
+    }
+
+    public void checkDeliveryMan(User user) {
+        if(user.getUserRole() != UserRole.DELIVERYMAN) {
+            throw new RuntimeException();
+        }
+    }
 }
