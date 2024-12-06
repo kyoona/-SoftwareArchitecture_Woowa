73a4b061abe6cb509488f0d2d04534f8f6dfaec5
leekeon215 <leekeon215@gmail.com>
2024-12-07 02:38:14 +0900

    feat: 결제 서비스 테스트 추가

--- payment/src/main/java/sa/repository/PaymentMethodRepository.java
+++ payment/src/main/java/sa/repository/PaymentMethodRepository.java
@@ -3,7 +3,6 @@ package sa.repository;
 public interface PaymentMethodRepository<T> {
 
     public T save(T paymentMethod);
-    public boolean exists(T paymentMethod);
+
     public T findByUserId(Long userId);
-    public T delete(T paymentMethod);
 }
